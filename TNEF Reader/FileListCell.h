78deaab1d11b1e16128f31a309ece63f46fa113a
Arne Schmitz <arne.schmitz@gmx.net>
2012-04-01 22:49:04 +0200

    add custom file list cell

---
+++ TNEF Reader/FileListCell.h
@@ -0,0 +1,13 @@
+//
+//  FileListCell.h
+//  TNEF Reader
+//
+//  Created by Arne Schmitz on 01.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileListCell : UITableViewCell
+
+@end
---
+++ TNEF Reader/FileListCell.m
@@ -0,0 +1,29 @@
+//
+//  FileListCell.m
+//  TNEF Reader
+//
+//  Created by Arne Schmitz on 01.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FileListCell.h"
+
+@implementation FileListCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
